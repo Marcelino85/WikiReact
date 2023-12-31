60eefa36e31ab1d39cd0dbf7f8bd4f0705d46780
Marcelino <marcelino85albuquerque@gmail.com>
2023-12-31 18:36:39 -0300

    Conclusão do projeto, onde retorna o repositorio especifico do usuario no GitHub

---
+++ src/components/Button/styles.js
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+export const ButtonContainer = styled.button`
+        background:#FAFAFA
+        border:1px solid #FAFAFA;
+        border-radius:20px;
+        
+        height:62px;
+        width:80%;
+
+        margin:20px;
+        transition: 0.5s;
+
+        &:hover{
+            background:#FAFAFA40;
+            color:#fff;
+            transition: 0.5s;
+            cursor:pointer;
+        }
+
+      
+`
